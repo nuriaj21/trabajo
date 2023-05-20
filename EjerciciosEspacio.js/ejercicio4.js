46e0bc592ad69fcdc087c7116bd1c8feb8c0f17e
milanegrin <lopezajoseg@gmail.com>
2023-05-20 16:47:17 -0500

    for suma de pares e impares

---
+++ EjerciciosEspacio.js/ejercicio4.js
@@ -0,0 +1,21 @@
+/*4.Sumar los números del 1 al 100. Sumar con condiciones, 
+solo los pares, solos los impares.
+El bucle debe poder sumar a cualquier n que se le indique.*/
+/*
+5 impares
+1+0
+1+ 3 = 4
+4 + 5 = 9*/
+let pares = 0;
+let impares = 0;
+let num = prompt("Introduzca un número")
+        
+        for( let i = 1; i <= num; i++){
+            if (i % 2 == 0){
+                pares += i;
+            } else {
+                impares += i;
+               }
+       
+        }
+   console.log(`Suma de Pares: ${pares} Suma de impares: ${impares} `);
