728cd4ce20943626bd063c5e329f49c2afb7dfa5
Milanegrin <adriana.negrin.17@gmail.com>
2023-05-21 12:02:17 -0500

    descuento

--- EjerciciosEspacio.js/ejercicio4.js
+++ EjerciciosEspacio.js/ejercicio4.js
@@ -9,37 +9,36 @@ El bucle debe poder sumar a cualquier n que se le indique.*/
 let pares = 0;
 let impares = 0;
 let num = prompt("Introduzca un número")
-        
-        for( let i = 1; i <= num; i++){
-            if (i % 2 == 0){
-                pares += i;
-            } else {
-                impares += i;
-               }
-       
-        }
-   console.log(`Suma de Pares: ${pares} Suma de impares: ${impares} `);
+
+for (let i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+        pares += i;
+    } else {
+        impares += i;
+    }
+
+}
+console.log(`Suma de Pares: ${pares} Suma de impares: ${impares} `);
 
 
-   /*Con funciones*/
-
-   function sumarNum(num, opc){
+/*Con funciones*/
+function sumarNum(num, opc) {
     let suma = 0;
-    if (opc == 'pares'){
-        for(let i = 1; i<= num; i++){
-            if (i % 2 == 0){
+    if (opc == 'pares') {
+        for (let i = 1; i <= num; i++) {
+            if (i % 2 == 0) {
                 suma += i;
                 console.log(`Pares de ${suma}`);
-        } 
-       }
-   }
-   if(opc == 'impares'){
-    for(let i = 1; i<= num; i++){
-        if(i % 2 != 0){
-            suma += i;
-            console.log(`Impares: ${suma}`);
+            }
         }
     }
-}
-return suma;
+    if (opc == 'impares') {
+        for (let i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                suma += i;
+                console.log(`Impares: ${suma}`);
+            }
+        }
+    }
+    return suma;
 }
